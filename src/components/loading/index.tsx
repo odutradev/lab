10118407f98b2f8e2d8a60276d855389ac70aebf
dutra-pc <joao.vitornl@gmail.com>
2024-08-17 20:42:51 -0300

    :sparkles: feat: Create loading component

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,27 @@
+import { Box, CircularProgress, Typography } from '@mui/material';
+
+const Loading = ({ showSpinner = true, message = "Loading..." }) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        width: '100%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: 2
+      }}
+    >
+      {showSpinner && <CircularProgress />}
+      <Typography variant="h6" sx={{ mt: 2 }}>
+        {message}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Loading;

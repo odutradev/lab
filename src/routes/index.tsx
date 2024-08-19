466e912ef72c8b14d69e0dce1758a57ffa71946c
dutra-pc <joao.vitornl@gmail.com>
2024-08-18 22:48:49 -0300

    :sparkles: feat: Create 404 page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate, Outlet} from "react-router-dom"
 import Dashboard from "../pages/dashboard";
 import SignIn from "../pages/signIn";
 import SignUp from "../pages/signUp";
+import Error from "../pages/error";
 
 const Router = () => {
     const PrivateRoute = () => localStorage.getItem("token") != null ? <Outlet  /> : <Navigate to="/signin" />;
@@ -10,10 +11,11 @@ const Router = () => {
     return(
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SignIn/>}/>
+          <Route path="/" element={<Navigate to="/signin" replace/>}/>
+          <Route path="*" element={<Navigate to="/404" replace/>}/>
           <Route path="/signin" element={<SignIn/>}/>
           <Route path="/signup" element={<SignUp/>}/>
-
+          <Route path="/404" element={<Error />}/>
 
           <Route element={<PrivateRoute  />}>
            <Route path="/dashboard" element={<Dashboard/>}/>

163d2f2e801578032f47d84da017d0aff755bd4f
dutra-pc <joao.vitornl@gmail.com>
2024-08-31 01:30:13 -0300

    :sparkles: feat: Create upload image function

---
+++ src/services/uploadImage.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+interface ResponseError {
+    error: string;
+}
+
+const uploadImage = async (image: File | string): Promise<string | ResponseError> => {
+    try {
+        const config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://api.imgur.com/3/image',
+            headers: { 
+                'Authorization': 'Client-ID c6e822b79325f86', 
+                'Content-Type': 'text/plain'
+            },
+            data: image
+        };
+
+        if (typeof image === 'string') {
+            return image;
+        }
+
+        const response = await axios(config);
+        return response.data.data.link;
+    } catch (error) {
+        if (axios.isAxiosError(error) && error.response) {
+            return { error: error.response.data.error || 'Erro desconhecido' };
+        }
+        return { error: 'Erro na requisição' };
+    }
+};
+
+export default uploadImage;

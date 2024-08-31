6e59e82a3887de9ddaa236467afdb1e105ca35cb
dutra-pc <joao.vitornl@gmail.com>
2024-08-31 00:31:27 -0300

    :lipstick: Add loading styles

---
+++ src/components/loading/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    justify-content: center;
+    align-items: center;
+    min-height: 80vh;
+    display: flex;
+    height: 100%;
+    width: 100%;
+`;

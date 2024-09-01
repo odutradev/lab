a53cdc9aec52b11554427db6c68453af8dfd414f
dutra-pc <joao.vitornl@gmail.com>
2024-09-01 00:26:04 -0300

    :sparkles: feat: Create spaces page

---
+++ src/pages/spaces/index.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "../../components/layout";
+
+const Spaces = () => {
+    return (
+        <DashboardLayout title="ESPAÇOS">
+            <h1>espaços</h1>
+        </DashboardLayout>
+    );
+};
+
+export default Spaces;

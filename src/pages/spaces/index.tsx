a6a47d2319deacf5e8d97b4c6673c346d50deaa5
dutra-pc <joao.vitornl@gmail.com>
2024-09-01 23:04:09 -0300

    :sparkles: feat: Create spaces page

--- src/pages/spaces/index.tsx
+++ src/pages/spaces/index.tsx
@@ -1,9 +1,121 @@
+import { Box, Grid, Typography, Card, CardContent, Tabs, Tab } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
+
 import DashboardLayout from "../../components/layout";
+import useMenuStore from "../../store/menu";
 
 const Spaces = () => {
+    const { menu } = useMenuStore(x => x);
+    const [tabValue, setTabValue] = useState<number>(0);
+    const navigate = useNavigate();
+    const theme = useTheme();
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setTabValue(newValue);
+    };
+
+    const handleCardClick = (id: string) => {
+        navigate(`/space/${id}`);
+    };
+
+    const handleCreateSpace = () => {
+        navigate('/create-space');
+    };
+
+    const mySpaces = menu.spaces.filter(space => space.invite === false || space.invite === undefined);
+    const invitations = menu.spaces.filter(space => space.invite === true);
+
     return (
         <DashboardLayout title="ESPAÇOS">
-            <h1>espaços</h1>
+            <Box textAlign="center" p={2}>
+                <Tabs value={tabValue} onChange={handleTabChange} centered>
+                    <Tab label="Meus Espaços" />
+                    <Tab label="Convites" />
+                </Tabs>
+                <Box p={2} marginTop='25px'>
+                    {tabValue === 0 ? (
+                        <Grid container spacing={2} justifyContent="center">
+                            {mySpaces.length > 0 ? (
+                                <>
+                                    {mySpaces.map(space => (
+                                        <Grid item key={space.id} xs={12} sm={6} md={4} lg={3}>
+                                            <Card
+                                                onClick={() => handleCardClick(space.id)}
+                                                style={{
+                                                    cursor: 'pointer',
+                                                    display: 'flex',
+                                                    flexDirection: 'column',
+                                                    justifyContent: 'center',
+                                                    alignItems: 'center',
+                                                    height: '200px',
+                                                    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+                                                    borderRadius: 8
+                                                }}
+                                            >
+                                                <CardContent style={{ textAlign: 'center' }}>
+                                                    <Typography variant="h6">{space.name}</Typography>
+                                                </CardContent>
+                                            </Card>
+                                        </Grid>
+                                    ))}
+                                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                                        <Card
+                                            onClick={handleCreateSpace}
+                                            style={{
+                                                cursor: 'pointer',
+                                                display: 'flex',
+                                                flexDirection: 'column',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                height: '200px',
+                                                boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+                                                borderRadius: 8
+                                            }}
+                                        >
+                                            <CardContent style={{ textAlign: 'center' }}>
+                                                <AddIcon style={{ fontSize: 70, color: theme.palette.primary.main }} />
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                </>
+                            ) : (
+                                <Typography variant="h6">Você não tem nenhum espaço.</Typography>
+                            )}
+                        </Grid>
+                    ) : (
+                        invitations.length > 0 ? (
+                            <Grid container spacing={2} justifyContent="center">
+                                {invitations.map(space => (
+                                    <Grid item key={space.id} xs={12} sm={6} md={4} lg={3}>
+                                        <Card
+                                            onClick={() => handleCardClick(space.id)}
+                                            style={{
+                                                cursor: 'pointer',
+                                                display: 'flex',
+                                                flexDirection: 'column',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                height: '200px',
+                                                boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+                                                borderRadius: 8
+                                            }}
+                                        >
+                                            <CardContent style={{ textAlign: 'center' }}>
+                                                <Typography variant="h6">{space.name}</Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        ) : (
+                            <Typography variant="h6">Você não tem nenhum convite.</Typography>
+                        )
+                    )}
+                </Box>
+            </Box>
         </DashboardLayout>
     );
 };

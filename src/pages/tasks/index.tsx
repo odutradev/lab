ebfa42d4e3c7dc3e1811982b9fcf77bc4699a3ab
dutra-pc <joao.vitornl@gmail.com>
2024-10-30 23:29:37 -0300

    :construction: Create tasks base page

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,15 +1,19 @@
-import { Box, Grid, Typography, Card, CardContent, Tabs, Tab, Chip, Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button } from "@mui/material";
+import { Button, Card, Grid, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 
-import { getAllTasks, createTask, ITaskAndSubs } from "../../actions/task";
+import { getAllTasks, ITaskAndSubs } from "../../actions/task";
 import DashboardLayout from "../../components/layout";
 import useAction from "../../hooks/useAction";
-import useUserStore from "../../store/user";
 
 const Tasks = () => {
+    const [searchQuery, setSearchQuery] = useState<string>("");
     const [ tasks, setTasks ] = useState<ITaskAndSubs[]>([]);
 
-    const { user } = useUserStore(x => x);
+    const getTasks = async () => {
+        const response = await getAllTasks();
+        if ('error' in response) return;
+        setTasks(response);
+    };
 
     const handleCreateSpace = () => useAction({
         action: async () =>  {},
@@ -23,17 +27,39 @@ const Tasks = () => {
         }
     });
 
+    const filteredTasks = tasks.filter((task) =>
+        task.description.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
     useEffect(() => {
-        (async () => {
-            const response = await getAllTasks();
-            if ('error' in response) return;
-            setTasks(response);
-        })()
+        getTasks();
     }, [])
 
     return (
         <DashboardLayout title="TAREFAS">
-            <></>
+            <Grid container justifyContent="center" gap="1rem" style={{ marginTop: '25px', marginBottom: '25px' }}>
+                <TextField
+                    label="Pesquisar tarefa"
+                    variant="outlined"
+                    fullWidth
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    style={{ width: '80%' }}
+                />
+                <Button variant="contained" color="primary" sx={{ width: '15%' }}> Criar Tarefa </Button>
+            </Grid>
+            <Grid container justifyContent="center"  alignItems="center" flexDirection="column">
+                {
+                    filteredTasks && filteredTasks.map((task) => (
+                        <div style={{ padding: '20px'}}>
+                            {task.identificator} - {task.description}<br/>
+                            {task.priority}<br/>
+                            {task.status}
+                        </div>
+                    ))
+                }
+
+            </Grid>
         </DashboardLayout>
     );
 };

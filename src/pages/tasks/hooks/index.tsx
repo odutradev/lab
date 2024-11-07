a4df8310ccdefa91342b4c81c4984f23e5571f8a
dutra-pc <joao.vitornl@gmail.com>
2024-11-07 00:48:18 -0300

    :construction: Add tasks provider render

--- src/pages/tasks/hooks/index.tsx
+++ src/pages/tasks/hooks/index.tsx
@@ -1,160 +1,59 @@
-
-/*
-import React, { useContext, createContext, useState, useEffect } from 'react';
-
-import {
-  getCategoriesByCompanyId,
-  createCategory,
-  deleteCategory,
-  updateCategory,
-  updateCategoryOrder,
-} from '@/app/services/products/categoriesService.ts';
-import {
-  CategoriesContextProps,
-  CategoriesFilters,
-  CategoriesProps,
-  TasksContextProps,
-  TasksProps,
-} from './types';
-import useAccessCompany from '@/app/hooks/accessToken/useAccessCompany';
+import React, { useContext, createContext, useState, useEffect, useCallback } from 'react';
+import { TasksContextProps, TasksProps } from './types';
 import { defaultValues } from './defaultValues';
-import Layout from '@/app/components/Layout';
+import { getAllTasks } from '../../../actions/task';
 
 const initialConfig = {
   ...defaultValues,
   updateState: () => {},
 };
 
-const CategoriesContext = createContext<TasksContextProps>(initialConfig);
+const TasksContext = createContext<TasksContextProps>(initialConfig);
 
 export const TasksProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-
   const [state, setState] = useState<TasksProps>(defaultValues);
 
-  const updateState = (newState: Partial<TasksProps>) => {
-    setState((prevState: TasksProps) => ({
+  const updateState = useCallback((newState: Partial<TasksProps>) => {
+    setState((prevState) => ({
       ...prevState,
       ...newState,
     }));
-  };
+  }, []);
 
-  const updateCategoriesOrder = async (newState: {
-    [status: string]: Category[];
-  }) => {
+  const getTasksData = async () => {
     try {
-      const mappedItems: Category[] = [];
-
-      Object.keys(newState).map((status) =>
-        newState[status].map((category, index) =>
-          mappedItems.push({
-            ...category,
-            order: index + 1,
-            active: status === 'active',
-          }),
-        ),
-      );
-
       updateState({ loading: true });
-
-      updateCategoryOrder(mappedItems).then(() => getCategoriesData({}));
-
-      updateState({ loading: false });
-
-      Layout.Notify({
-        message: 'Categoria atualizada!',
-        type: 'success',
-        theme: 'light',
+      const response = await getAllTasks();
+      if ('error' in response) return;
+      
+      updateState({
+        defaultTasks: response,
+        tasks: response,
+        loading: false,
       });
     } catch (error) {
       updateState({ loading: false });
-
-      Layout.Notify({
-        message: 'Erro ao atualizar a categoria!',
-        type: 'error',
-        theme: 'dark',
-      });
-    }
-  };
-
-  const getCategoriesData = async ({
-    ignoreRequest,
-    filters,
-  }: {
-    ignoreRequest?: boolean;
-    filters?: CategoriesFilters;
-  }) => {
-    try {
-      updateState({ loading: true });
-
-      if (!ignoreRequest) {
-        const response = await getCategoriesByCompanyId(companyId, '');
-
-        updateState({
-          loading: false,
-          defaultCategories: response,
-          rawCategories: response,
-        });
-      } else {
-        const searchText = (filters?.search as string)?.trim().toLowerCase();
-
-        const applyTextFilter = (categories: typeof state.rawCategories) =>
-          categories.filter(
-            (category) =>
-              !searchText ||
-              (category.name as string).toLowerCase().includes(searchText),
-          );
-
-        const defaultCategoriesResponse = applyTextFilter(state.rawCategories);
-
-        updateState({
-          loading: false,
-          filters,
-          defaultCategories: defaultCategoriesResponse,
-        });
-      }
-    } catch (error) {
-      updateState({ loading: false });
-
-      Layout.Notify({
-        message: 'Não foi possível obter as categorias',
-        type: 'error',
-        theme: 'dark',
-      });
     }
   };
 
   useEffect(() => {
-    if (accessCompany && accessCompany.id) {
-      setCompanyId(accessCompany.id);
-    }
-  }, [accessCompany]);
-
-  useEffect(() => {
-    if (companyId) {
-      getCategoriesData({});
-    }
-  }, [companyId]);
+    getTasksData();
+  }, []);
 
   const value = {
     ...state,
-    updateCategoriesOrder,
-    createCategoryHandler,
-    deleteCategoryHandler,
-    updateCategoryHandler,
-    updateFilters,
     updateState,
   };
 
   return (
-    <CategoriesContext.Provider value={value}>
+    <TasksContext.Provider value={value}>
       {children}
-    </CategoriesContext.Provider>
+    </TasksContext.Provider>
   );
 };
 
-const useCategory = () => useContext(CategoriesContext);
+const useTasks = () => useContext(TasksContext);
 
-export default useCategory;
-*/
+export default useTasks;
--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,115 +1,121 @@
-import {  Grid } from "@mui/material";
-import { useEffect, useState } from "react";
-import { DndContext, closestCenter } from "@dnd-kit/core";
+import { Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { DndContext, closestCenter } from '@dnd-kit/core';
 import {
   SortableContext,
   verticalListSortingStrategy,
   arrayMove,
-} from "@dnd-kit/sortable";
+} from '@dnd-kit/sortable';
 
-
-import { getAllTasks, ITaskAndSubs, updateAllTasks } from "../../actions/task";
-import DashboardLayout from "../../components/layout";
-import Task from "./components/task";
-import Column from "./components/column";
-import { TaskStatus, TaskStatusTypes } from "./types";
-import Overlay from "./components/overlay";
-import Empty from "./components/empty";
+import { updateAllTasks } from '../../actions/task';
+import DashboardLayout from '../../components/layout';
+import Task from './components/task';
+import Column from './components/column';
+import { TaskStatus, TaskStatusTypes } from './types';
+import Overlay from './components/overlay';
+import Empty from './components/empty';
+import useTasks, { TasksProvider } from './hooks';
 
 const Tasks = () => {
-  const [tasks, setTasks] = useState<ITaskAndSubs[]>([]);
-  const [activeTaskId, setActiveTaskId] = useState<null | string>(null);
-
-  const getTasks = async () => {
-    const response = await getAllTasks();
-    if ("error" in response) return;
-    setTasks(response);
-  };
-
-  const statusCategories = ["inactive",, "blocked", "pending", "active", "completed"];
-  const tasksByStatus = statusCategories.map((status) => ({
-    status,
-    tasks: tasks.filter((task) => task.status === status),
-  }));
-
-  const handleDragStart = ({ active }: any) => {
-    setActiveTaskId(active.id);
-  };
-
-  const handleDragEnd = (event: any) => {
-    const { active, over } = event;
-
-    if (!over) return;
-
-    const oldIndex = tasks.findIndex((task) => task.identificator === active.id);
-    const newStatus = over.data?.current?.sortable.containerId;
-
-    if (newStatus && newStatus !== tasks[oldIndex].status) {
-      setTasks((prevTasks) => {
-        const updatedTasks = prevTasks.map((task) =>
-          task.identificator === active.id ? { ...task, status: newStatus } : task
+  
+  const TasksRender = () => {
+      const [activeTaskId, setActiveTaskId] = useState<null | string>(null);
+      const { tasks, updateState } = useTasks();
+    
+      const statusCategories = ['inactive', 'blocked', 'pending', 'active', 'completed'];
+      const tasksByStatus = statusCategories.map((status) => ({
+        status,
+        tasks: tasks.filter((task) => task.status === status),
+      }));
+    
+      const handleDragStart = ({ active }: any) => {
+        setActiveTaskId(active.id);
+      };
+    
+      const handleDragEnd = (event: any) => {
+        const { active, over } = event;
+    
+        if (!over) return;
+    
+        const oldIndex = tasks.findIndex((task) => task.identificator === active.id);
+        const newStatus = over.data?.current?.sortable.containerId;
+    
+        if (newStatus && newStatus !== tasks[oldIndex].status) {
+          // Atualizar o status do task diretamente
+          updateState({
+            tasks: tasks.map((task) =>
+              task.identificator === active.id ? { ...task, status: newStatus } : task
+            ),
+          });
+          return;
+        }
+    
+        const newIndex = tasks.findIndex((task) => task.identificator === over.id);
+        if (newIndex !== -1 && oldIndex !== newIndex) {
+          const reorderedTasks = arrayMove(tasks, oldIndex, newIndex);
+          updateState({
+            tasks: reorderedTasks.map((task, index) => ({
+              ...task,
+              order: index + 1,
+            })),
+          });
+        }
+    
+        setActiveTaskId(null);
+      };
+    
+      useEffect(() => {
+        tasksByStatus.forEach((item) =>
+          item.tasks.forEach((task, index) => {
+            const taskIndex = tasks.findIndex((x) => x._id === task._id);
+            tasks[taskIndex].order = index;
+            tasks[taskIndex].status = item.status as TaskStatusTypes;
+            updateState({ tasks });
+            updateAllTasks(tasks);
+          })
         );
-        return updatedTasks;
-      });
-      return;
-    }
+      }, [tasksByStatus, updateState]);
+    
+    
+      return (
+        <DashboardLayout title="TAREFAS">
+          <TasksProvider>
+            <DndContext
+              collisionDetection={closestCenter}
+              onDragStart={handleDragStart}
+              onDragEnd={handleDragEnd}
+            >
+              <Grid container spacing={2} justifyContent="space-around">
+                {tasksByStatus.map(({ status, tasks }) => (
+                  <Column title={TaskStatus[status as keyof typeof TaskStatus]}>
+                    <SortableContext
+                      items={tasks.map((task) => task.identificator)}
+                      id={status}
+                      strategy={verticalListSortingStrategy}
+                    >
+                      {tasks.length > 0 ? (
+                        tasks
+                          .sort((a, b) => (a.order || 0) - (b.order || 0))
+                          .map((task) => <Task key={task._id} task={task} />)
+                      ) : (
+                        <Empty id={status as string} />
+                      )}
+                    </SortableContext>
+                  </Column>
+                ))}
+              </Grid>
+              <Overlay tasks={tasks} activeTaskId={activeTaskId} />
+            </DndContext>
+          </TasksProvider>
+        </DashboardLayout>
+      );
 
-    const newIndex = tasks.findIndex((task) => task.identificator === over.id);
-    if (newIndex !== -1 && oldIndex !== newIndex) {
-      const reorderedTasks = arrayMove(tasks, oldIndex, newIndex);
-      setTasks(reorderedTasks.map((task, index) => ({
-        ...task,
-        order: index + 1,
-      })));
-    }
-
-    setActiveTaskId(null);
   };
-
-  useEffect(() => {
-      tasksByStatus.forEach((item) => item.tasks.forEach((task, index) => {
-        let taskIndex = tasks.findIndex(x => x._id == task._id);
-        var reorderedTasks = tasks;
-        tasks[taskIndex].order = index;
-        tasks[taskIndex].status = item.status as TaskStatusTypes;
-        setTasks(reorderedTasks)
-        updateAllTasks(reorderedTasks);
-      }))
-  }, [tasksByStatus])
-
-
-
-  useEffect(() => {
-    getTasks();
-  }, []);
 
   return (
-    <DashboardLayout title="TAREFAS">
-      <DndContext
-        collisionDetection={closestCenter}
-        onDragStart={handleDragStart}
-        onDragEnd={handleDragEnd}
-      >
-        <Grid container spacing={2} justifyContent="space-around">
-          {tasksByStatus.map(({ status, tasks }) => (
-            <Column title={TaskStatus[status as keyof typeof TaskStatus]}>
-                <SortableContext
-                  items={tasks.map((task) => task.identificator)}
-                  id={status}
-                  strategy={verticalListSortingStrategy}
-                >
-                  {tasks.length > 0 ? (
-                    tasks.sort((a, b) => (a.order || 0) -(b.order || 0)).map((task) => <Task key={task._id} task={task} />)
-                  ) : (
-                    <Empty id={status as string} />
-                  )}
-                </SortableContext>
-            </Column>
-          ))}
-        </Grid>
-        <Overlay tasks={tasks} activeTaskId={activeTaskId} />
-      </DndContext>
-    </DashboardLayout>
+    <TasksProvider>
+      <TasksRender />
+    </TasksProvider>
   );
 };
 

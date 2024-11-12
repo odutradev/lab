c8aaa7eabc298077822a6c459da9c8a3daff5700
dutra-pc <joao.vitornl@gmail.com>
2024-11-12 01:21:04 -0300

    :sparkles: feat: Update task card styles

--- src/pages/tasks/components/task/index.tsx
+++ src/pages/tasks/components/task/index.tsx
@@ -1,42 +1,55 @@
-import { Card, CardContent, Typography, Box, Chip } from "@mui/material";
+import { Card, Typography, Chip, IconButton } from "@mui/material";
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
+import { BoxContent, ChipsContainer, Content, DraggableTask, IconContainer, TitleContainer } from "./styles";
 import { ITask, ITaskAndSubs } from "../../../../actions/task";
-import { TaskStatus } from "../../types";
-import { DraggableTask } from "./styles";
   
-const Task = ({ task, isOverlay } : { task: ITask | ITaskAndSubs, isOverlay?: boolean}) => {
-
+const Task = ({ task, isOverlay }: { task: ITask | ITaskAndSubs, isOverlay?: boolean}) => {
     const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id: task._id });
 
     const style = {
       transform: CSS.Transform.toString(transform),
       transition,
     };
-  
+
+    const priorityLabels: { [key: string]: string } = {
+        high: "Alta",
+        medium: "Média",
+        low: "Baixa"
+    };
+
     return (
-        <DraggableTask isDragging={isDragging} isOverlay={isOverlay} ref={setNodeRef} style={style} {...attributes} {...listeners}>
-            <Card variant="outlined" sx={{ borderRadius: 2, mb: 2, p: 1 }}>
-                <CardContent>
-                    <Box display="flex" justifyContent="space-between" alignItems="center" mb={1}>
-                        <Typography variant="h6" color="primary">
-                            {task.identificator}
-                        </Typography>
-                        <Chip 
-                            label={TaskStatus[task.status as keyof typeof TaskStatus].charAt(0).toUpperCase() + TaskStatus[task.status as keyof typeof TaskStatus].slice(1)} 
-                            color={task.status === 'completed' ? 'success' : task.status === 'pending' ? 'warning' : 'default'}
-                            variant="outlined"
-                            size="small"
-                        />
-                    </Box>
-                    <Typography variant="body1" gutterBottom>
-                        {task.description}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                        Prioridade: <strong>{task.priority}</strong>
-                    </Typography>
-                </CardContent>
+        <DraggableTask isDragging={isDragging} isOverlay={isOverlay} ref={setNodeRef} style={style} {...attributes}>
+            <Card variant="outlined" sx={{ borderRadius: 2, mb: 2, p: 2 }}>
+                    <BoxContent>
+                        <IconContainer>
+                            <IconButton {...listeners} {...attributes} sx={{ mr: 1 }}>
+                                <DragIndicatorIcon />
+                            </IconButton>
+                        </IconContainer>
+                        <Content>
+                            <TitleContainer>
+                                    <Typography variant="h6" color="primary" sx={{ paddingRight: "15px"}}>
+                                        {task.identificator}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        {task.description}
+                                    </Typography>
+                            </TitleContainer>
+
+                        <ChipsContainer>
+                            <Chip 
+                                label={priorityLabels[task.priority]} 
+                                color={task.priority === 'high' ? 'error' : task.priority === 'medium' ? 'warning' : 'default'}
+                                variant="outlined"
+                                size="small"
+                                />
+                        </ChipsContainer>
+                        </Content>
+
+                </BoxContent>
             </Card>
         </DraggableTask>
     );
--- src/pages/tasks/components/task/styles.ts
+++ src/pages/tasks/components/task/styles.ts
@@ -6,3 +6,41 @@ export const DraggableTask  = styled.div<{ isDragging: boolean, isOverlay?: bool
   cursor: ${({ isDragging }) => (isDragging ? "grabbing" : "auto")};
   width: 100%;
 `;
+
+export const BoxContent = styled.div`
+  flex-direction: row;
+  align-items: center;
+  display: flex;
+  flex: 1;
+`;
+
+export const IconContainer = styled.div`
+  height: 100%;
+  width: auto;
+`;
+
+export const TitleContainer = styled.div`
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  display: flex;
+  width: 100%;
+  `;
+
+export const Content = styled.div`
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+  display: flex;
+  flex: 1;
+`;
+
+export const ChipsContainer = styled.div`
+  justify-content: flex-end;
+  flex-direction: row;
+  align-items: center;
+  padding-top: 25px;
+  display: flex;
+  width: 100%;
+  gap: 2.5px;
+`;

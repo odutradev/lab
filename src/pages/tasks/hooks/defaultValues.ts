444671f6a6ec28231e736469c9bb562f83c116a3
dutra-pc <joao.vitornl@gmail.com>
2024-11-12 00:32:06 -0300

    :bug: fix: Fix build error

--- src/pages/tasks/hooks/defaultValues.ts
+++ src/pages/tasks/hooks/defaultValues.ts
@@ -3,7 +3,6 @@ import { TasksProps } from './types';
 export const statusCategories = ['inactive', 'blocked', 'pending', 'active', 'completed'];
 
 export const defaultValues: TasksProps = {
-    createModal: false,
     defaultTasks: [],
     tasksByStatus: [],
     loading: false,
